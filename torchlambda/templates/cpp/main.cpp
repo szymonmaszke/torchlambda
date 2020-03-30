f4fe0fa20f7daf414273bf3f6d82e7be269f4be9
vyzyv <vyz@protonmail.com>
2020-03-30 14:58:23 +0200

    Improve CPP code template

--- torchlambda/templates/cpp/main.cpp
+++ torchlambda/templates/cpp/main.cpp
@@ -11,18 +11,22 @@
 #include <torch/script.h>
 #include <torch/torch.h>
 
+/*!
+ *
+ *                    HANDLE REQUEST
+ *
+ */
+
 static aws::lambda_runtime::invocation_response
 handler(torch::jit::script::Module &module,
         const Aws::Utils::Base64::Base64 &transformer,
         const aws::lambda_runtime::invocation_request &request) {
 
-  /* Name of field containing base64 encoded data */
   const Aws::String data_field{"data"};
 
   /*!
    *
-   *               PARSE AND VALIDATE REQUEST
-   *        (you may want to add more validations)
+   *              PARSE AND VALIDATE REQUEST
    *
    */
 
@@ -45,35 +49,34 @@ handler(torch::jit::script::Module &module,
   const auto base64_data = json_view.GetString(data_field);
   Aws::Utils::ByteBuffer decoded = transformer.Decode(base64_data);
 
-  /* Create tensor of type Byte from pointer and cast to float32 */
   torch::Tensor tensor =
       torch::from_blob(decoded.GetUnderlyingData(),
                        {
                            static_cast<long>(decoded.GetLength()),
                        },
                        torch::kUInt8)
-          /* You may want to pass those values in your request*/
-          /* if tensors with dynamic shapes are provided */
           .reshape({1, 3, 64, 64})
           .toType(torch::kFloat32) /
       255.0;
 
-  /* Normalize tensor with ImageNet means and standard deviations */
   torch::Tensor normalized_tensor = torch::data::transforms::Normalize<>{
       {0.485, 0.456, 0.406}, {0.229, 0.224, 0.225}}(tensor);
 
   /*!
    *
-   *              MAKE INFERENCE AND RETURN JSON RESPONSE
+   *                      MAKE INFERENCE
    *
    */
 
-  /* {} will be casted to std::vector<torch::jit::IValue> under the hood */
   auto output = module.forward({normalized_tensor}).toTensor();
-  /* As only single element will be outputted from argmax we can use item */
   const int label = torch::argmax(output).item<int>();
 
-  /* Return JSON with field label containing predictions*/
+  /*!
+   *
+   *                       RETURN JSON
+   *
+   */
+
   return aws::lambda_runtime::invocation_response::success(
       Aws::Utils::Json::JsonValue{}
           .WithInteger("label", label)
@@ -83,16 +86,17 @@ handler(torch::jit::script::Module &module,
 }
 
 int main() {
-  /* Inference doesn't usually need gradient, let's turn it off */
-  torch::NoGradGuard no_grad_guard{};
+  /*!
+   *
+   *                        LOAD MODEL ON CPU
+   *                    & SET IT TO EVALUATION MODE
+   *
+   */
 
-  /* Layers are unpacked to /opt if you are passing your model this way */
+  torch::NoGradGuard no_grad_guard{};
   constexpr auto model_path = "/opt/model.ptc";
 
-  /* You could add some checks whether the module is loaded correctly */
-  /* Module should be loaded here for faster inference and shared via context */
   torch::jit::script::Module module = torch::jit::load(model_path, torch::kCPU);
-
   module.eval();
 
   /*!
