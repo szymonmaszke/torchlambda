49a5f9d6c14e0d37d734f755e94c83b3f79b0fe2
vyzyv <vyz@protonmail.com>
2020-04-02 01:54:40 +0200

    Change nightly to daily version update

--- scripts/release/update_version.sh
+++ scripts/release/update_version.sh
@@ -3,4 +3,3 @@
 repository="$(basename "$(git rev-parse --show-toplevel)")"
 # Version equal to current timestamp
 echo "__version__ = \"$(date +%s)\"" >"$repository"/_version.py
-echo "_name = \"$repository-nightly\"" >"$repository"/_name.py
