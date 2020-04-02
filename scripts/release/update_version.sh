e3723f953ddd8b03fb125948c3f685f3767a0421
vyzyv <vyz@protonmail.com>
2020-04-02 02:52:16 +0200

    Verbose change version and print full path

--- scripts/release/update_version.sh
+++ scripts/release/update_version.sh
@@ -4,6 +4,11 @@ set -e
 
 repository="$(basename "$(git rev-parse --show-toplevel)")"
 # Version equal to current timestamp
+echo "Current working directory: $(pwd)"
+echo "OLD VERSION CHECK:"
 cat "$repository"/_version.py
+
 echo "__version__ = \"$(date +%s)\"" >"$repository"/_version.py
+
+echo "NEW VERSION:"
 cat "$repository"/_version.py
