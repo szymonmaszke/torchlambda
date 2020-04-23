6d32a9908a2e32959250879654423bcd7c11dbd4
vyzyv <vyz@protonmail.com>
2020-04-23 17:52:07 +0200

    Pin "latest" as default build version

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-TORCH_VERSION=$1
+TORCH_VERSION=${1:-"latest"}
 torchlambda build . --no-run --pytorch-version "$TORCH_VERSION" --image "szymonmaszke/torchlambda:$TORCH_VERSION"
