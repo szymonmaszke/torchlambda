fa34532bce7c80c5a68a49e0a8dfac4e9669ed95
vyzyv <vyz@protonmail.com>
2020-04-24 18:33:45 +0200

    Point users to documentation

--- torchlambda/arguments/parser.py
+++ torchlambda/arguments/parser.py
@@ -5,13 +5,17 @@ from .subparsers import build, layer, settings, template
 
 def get():
     """Get user provided arguments."""
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="Lightweight tool to deploy PyTorch models on AWS Lambda.\n"
+        "For more information see documentation: https://github.com/szymonmaszke/torchlambda/wiki",
+    )
 
     parser.add_argument(
         "--silent",
         required=False,
         action="store_true",
-        help="Will only output most basic information (e.g. info from build won't be displayed).\n"
+        help="Will only output torchlambda information (e.g. building PyTorch output won't be displayed).\n"
         "Default: False",
     )
 
